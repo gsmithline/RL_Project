ceacff8dcc2d14f48325028c80ea235911680f2a
Gabe Smithline <63967324+gsmithline@users.noreply.github.com>
2024-04-04 18:30:54 -0400

    works but its really crappy

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import random
 import pandas as pd
 from ma_gym.envs.traffic_junction import TrafficJunction  
 from collections import defaultdict
+import nashpy as nash
 
-#info_df = pd.
-generations = 100
+info_df = pd.DataFrame()
+generations = 2
 
 '''
 Policy Initialization Randomly
@@ -34,8 +35,22 @@ def simulate_episode(env, policies):
 
     return cumulative_rewards
 
+def compute_nash_equilibrium(meta_game_payoffs):
+    equilibrium_policies = {}
+    for match, payoff_matrix in meta_game_payoffs.items():
+        game = nash.Game(payoff_matrix[0], payoff_matrix[1])
+        eqs = list(game.support_enumeration())
+        equilibrium_policies[match] = eqs
+    return equilibrium_policies
 
-    
+def create_policy_from_equilibrium(equilibrium, action_space_size):
+    # This function creates a policy function from a given Nash equilibrium distribution
+    def policy(observation):
+        action_probabilities = equilibrium  # Assuming equilibrium is a distribution over actions
+        action = np.random.choice(np.arange(action_space_size), p=action_probabilities)
+        return action
+    return policy
+
 
 def psro_simulation(env, generations=generations, episodes_per_matchup=10):
     num_agents = env.n_agents
@@ -56,6 +71,30 @@ def psro_simulation(env, generations=generations, episodes_per_matchup=10):
                     meta_game_payoffs[(agent1, agent2)] += np.array(cumulative_rewards).reshape((num_agents, 1, 1))
                     meta_game_payoffs[(agent2, agent1)] += np.array(cumulative_rewards).reshape((num_agents, 1, 1))
 
+        nash_equilibrium_policies = compute_nash_equilibrium(meta_game_payoffs)
+
+        policies = {}
+        
+        for agent1 in range(num_agents):
+            for agent2 in range(agent1 + 1, num_agents):
+                if (agent1, agent2) in nash_equilibrium_policies:
+                    equilibrium = random.choice(nash_equilibrium_policies[(agent1, agent2)]) #in case of multiple equilibriums
+                    policies[agent1] = create_policy_from_equilibrium(equilibrium[0], action_space_size)
+                    policies[agent2] = create_policy_from_equilibrium(equilibrium[1], action_space_size)
+        
+        
+    return policies
+ 
+def run_computed_policies(env, policies):
+    observations = env.reset()
+    done = [False] * env.n_agents
+    while not all(done):
+        actions = [policies[agent](observations[agent]) for agent in range(env.n_agents)]
+        observations, rewards, done, info = env.step(actions)
+        env.render()
+
+
+        
 
 gym.envs.register(
     id='TrafficJunction10-v0',
@@ -63,5 +102,7 @@ gym.envs.register(
     kwargs={'max_steps': 1000}
 )
 env = gym.make('TrafficJunction10-v1')
-psro_simulation(env)
+policies = psro_simulation(env)
+run_computed_policies(env, policies)
+
 env.close()
